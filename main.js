cc4a55a99ae1ddf3dbfb456137c48d55b43e5cd7
Delovo1 <maxsim.0981@gmail.com>
2025-10-18 21:17:09 +0300

    making disign for header and buttons

--- main.js
+++ main.js
@@ -1,38 +1,3 @@
-// function memoize(fn) {
-//   //
-//   const cache = new Map();
-//   return (...args) => {
-//     const key = JSON.stringify(args);
-//     if (cache.has(key)) {
-//       return cache.get(key);
-//     }
-//     const result = fn(...args);
-//     cache.set(key, result);
-//     return result;
-//   };
-// }
-
-// // Объект calculator не меняем
-// const calculator = {
-//   power(num, exponent = 2) {
-//     return Math.pow(num, exponent);
-//   },
-//   fibonacci(n) {
-//     if (n <= 1) return n;
-//     return this.fibonacci(n - 1) + this.fibonacci(n - 2);
-//   },
-//   factorial(n) {
-//     return n === 0 ? 1 : n * this.factorial(n - 1);
-//   },
-// };
-
-// function a(data) {
-//   const { methodName, args } = JSON.parse(data.toString());
-//   const memoFunc = memoize(calculator[methodName]);
-//   console.log(memoFunc(...args)); // рассчет
-//   console.log(memoFunc(...args)); // должны получить из кэша
-// }
-// a(`{"methodName": "factorial", "args": [5]}`);
 const div = document.querySelector(".todolist");
 const btnAdd = document.querySelector(".addTodo");
 const btnRemove = document.querySelector(".removeTodo");
@@ -44,22 +9,21 @@ btnAdd.addEventListener("click", () => {
     const label = document.createElement("label");
     input.type = "checkbox";
     label.textContent = `${inputOfUser}`;
-
-    div.append(label);
-    label.append(input);
+    const task = document.createElement("div");
+    task.className = "task";
+    task.append(input);
+    task.append(label);
+    div.append(task);
     input.id = `${String(i)}`;
     label.htmlFor = `${String(i++)}`;
-    div.append(document.createElement("br"));
   }
 });
 btnRemove.addEventListener("click", () => {
   const arrayOfInputs = div.querySelectorAll("input");
   for (const element of arrayOfInputs) {
     if (element.checked) {
-      element.parentElement.nextSibling.remove();
+      div.querySelector(`label[for ="${element.id}"]`).remove();
       element.parentElement.remove();
-
-      // document.querySelector(`label[for ="[${element.name}]"`).remove();
     }
   }
 });
