6bb4c9eb9073368934138af71f5e5380159c645b
Delovo1 <maxsim.0981@gmail.com>
2025-10-19 17:11:54 +0300

    was made priority for tasks and was attempt to safe tasks after release

--- main.js
+++ main.js
@@ -2,9 +2,50 @@ const div = document.querySelector(".todolist");
 const btnAdd = document.querySelector(".addTodo");
 const btnRemove = document.querySelector(".removeTodo");
 let i = 1;
+function renderTodos() {
+  const arr = JSON.parse(localStorage.getItem("todos")) || [];
+  console.log(arr);
+  for (const element of arr) {
+    div.append(element);
+  }
+}
+function whatPriotity(priority) {
+  const prior = +prompt("Введите приоритет от 1 до 5 включительно");
+  if (prior === 1) {
+    priority.style.backgroundColor = "#57FF57";
+    priority.parentElement.setAttribute("pr", prior);
+  } else if (prior === 2) {
+    priority.style.backgroundColor = "#338033";
+    priority.parentElement.setAttribute("pr", prior);
+  } else if (prior === 3) {
+    priority.parentElement.setAttribute("pr", prior);
+    priority.style.backgroundColor = "#DADA26";
+  } else if (prior === 4) {
+    priority.parentElement.setAttribute("pr", prior);
+    priority.style.backgroundColor = "#E69E1A";
+  } else if (prior === 5) {
+    priority.parentElement.setAttribute("pr", prior);
+    priority.style.backgroundColor = "#D03030";
+  } else {
+    alert("Вводить нужно от 1 до 5!");
+    whatPriotity(priority);
+  }
+}
+function sortByPrior(task) {
+  div.append(task);
+  const array = Array.from(div.querySelectorAll(".task"));
+  array.sort((a, b) => b.getAttribute("pr") - a.getAttribute("pr"));
+  for (const element of array) {
+    div.append(element);
+  }
+  console.log(array);
+  localStorage.setItem("todos", JSON.stringify(array));
+}
 btnAdd.addEventListener("click", () => {
-  const inputOfUser = prompt("Введите название заметки");
+  const inputOfUser = prompt("Введите название заметки").trim();
   if (inputOfUser) {
+    const priority = document.createElement("div");
+    priority.className = "prior";
     const input = document.createElement("input");
     const label = document.createElement("label");
     input.type = "checkbox";
@@ -13,9 +54,11 @@ btnAdd.addEventListener("click", () => {
     task.className = "task";
     task.append(input);
     task.append(label);
-    div.append(task);
+    task.append(priority);
     input.id = `${String(i)}`;
     label.htmlFor = `${String(i++)}`;
+    whatPriotity(priority);
+    sortByPrior(task);
   }
 });
 btnRemove.addEventListener("click", () => {
@@ -26,4 +69,8 @@ btnRemove.addEventListener("click", () => {
       element.parentElement.remove();
     }
   }
+  const array = Array.from(div.querySelectorAll(".task"));
+  localStorage.setItem("todos", JSON.stringify(array));
+  console.log(JSON.stringify(array));
 });
+renderTodos();
