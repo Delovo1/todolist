3f3ff7412037e5ede2fc628effeda896a06c7d62
Delovo1 <maxsim.0981@gmail.com>
2025-10-20 22:24:34 +0300

    global update: safe tasks

--- main.js
+++ main.js
@@ -5,6 +5,21 @@ const burger = document.querySelector(".burger");
 const point = document.querySelector(".menu img");
 const buttons = document.querySelector(".buttons");
 const array = Array.from(div.querySelectorAll(".task"));
+const tasks = Array.from(div.querySelectorAll(".task"));
+let a = 0;
+function getObj() {
+  a = Array.from(document.querySelectorAll(".task")).map((val) => {
+    return {
+      element: "div",
+      className: val.className,
+      pr: val.getAttribute("pr"),
+      id: val.querySelector("input").getAttribute("id"),
+      info: `${val.querySelector("label").textContent}`,
+    };
+  });
+  localStorage.setItem("tasks", JSON.stringify(a));
+}
+
 array.forEach(
   (val) =>
     (val.style.marginLeft = `${
@@ -52,13 +67,44 @@ window.addEventListener("resize", () => {
     );
   }
 });
-
+function makepr(priority, prior) {
+  prior = Number(prior);
+  if (prior === 1) {
+    priority.style.backgroundColor = "#57FF57";
+  } else if (prior === 2) {
+    priority.style.backgroundColor = "#338033";
+  } else if (prior === 3) {
+    priority.style.backgroundColor = "#DADA26";
+  } else if (prior === 4) {
+    priority.style.backgroundColor = "#E69E1A";
+  } else if (prior === 5) {
+    priority.style.backgroundColor = "#D03030";
+  }
+}
+function makeElFromObg(obj) {
+  const elem = document.createElement(`div`);
+  const label = document.createElement("label");
+  const input = document.createElement("input");
+  const prior = document.createElement("div");
+  input.setAttribute("id", obj.id);
+  prior.className = "prior";
+  elem.className = `${obj.className}`;
+  elem.setAttribute("pr", `${obj.pr}`);
+  input.type = "checkbox";
+  label.textContent = obj.info;
+  label.setAttribute("for", obj.id);
+  elem.append(input);
+  elem.append(label);
+  elem.append(prior);
+  makepr(prior, obj.pr);
+  return elem;
+}
 let i = 1;
 function renderTodos() {
-  const arr = JSON.parse(localStorage.getItem("todos")) || [];
-  console.log(arr);
+  const arr = JSON.parse(localStorage.getItem("tasks")) || [];
+
   for (const element of arr) {
-    // div.append(element);
+    div.append(makeElFromObg(element));
   }
 }
 burger.addEventListener("mouseover", () => {
@@ -106,8 +152,8 @@ function sortByPrior(task) {
   for (const element of array) {
     div.append(element);
   }
-  console.log(array);
   localStorage.setItem("todos", JSON.stringify(array));
+  getObj();
 }
 btnAdd.addEventListener("click", () => {
   const inputOfUser = prompt("Введите название заметки").trim();
@@ -139,6 +185,6 @@ btnRemove.addEventListener("click", () => {
   }
   const array = Array.from(div.querySelectorAll(".task"));
   localStorage.setItem("todos", JSON.stringify(array));
-  console.log(JSON.stringify(array));
+  getObj();
 });
 renderTodos();
